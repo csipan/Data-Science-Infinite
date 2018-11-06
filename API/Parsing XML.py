57914cfc899af1973b683f96dc6be8351392c113
csipan <csipan_mihalyalbert@yahoo.com>
2018-11-06 14:30:00 +0100

    Parsing XML with urllib.request

--- API/Parsing XML.py
+++ API/Parsing XML.py
@@ -1,9 +1,29 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-source = urllib.request.urlopen("http://www.nemzetisport.hu/magyar_valogatott/marco-rossi-sohasem-volt-bajom-dzsudzsak-balazssal-2667547").read()
+source = urllib.request.urlopen("http://jira.greenfox.academy/secure/ViewProfile.jspa").read()
 
-# miért nem megy az idézőjelben az lxml
 soup = BeautifulSoup(source, "html.parser")
 
-print(soup.h1)
+# get the whole html format from the source
+print(soup.prettify())
+
+# get the first "title" (or others) with and without the title tag
+print(soup.title)
+print(soup.title.text)
+print(soup.a.text)
+
+# getting the info in a different way
+# if we use the class form the html, we have to add an underscore like below
+first_class = soup.find("fieldset", class_="parameters hidden")
+print(first_class.prettify())
+
+# getting deeper and deeper
+div = soup.find("div", id="footer-logo")
+print(div.a.text)
+
+# using find all argument and iterating true the whole html
+for ul in soup.find_all("ul", class_="atlassian-footer"):
+    ul_text = ul.li
+    print(ul_text.text)
+    print("-----------------------------------------------------------")
