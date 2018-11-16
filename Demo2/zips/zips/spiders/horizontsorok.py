1ead8d734f804b7785853a3f1f1994382d444bda
csipan <csipan_mihalyalbert@yahoo.com>
2018-11-16 13:10:36 +0100

    trying to get data from horizontsorok, but no success

--- Demo2/zips/zips/spiders/horizontsorok.py
+++ Demo2/zips/zips/spiders/horizontsorok.py
@@ -8,6 +8,16 @@ class HorizontsorokCrawler(scrapy.Spider):
     ]
 
     def parse(self, response):
-        beer_names = response.css("h3.s-component-content p::text").extract()
-        beer_types = response.css("div.s-component-content.s-font-body p::text").extract()
-        print(beer_names)
+        beer_names = response.css("div.container div.s-repeatable-item div.s-item-title div.s-text")
+        for item in beer_names:
+            print(item.extract())
+        # beer_types = response.css("div.s-component-content.s-font-body p::text").extract()
+        # print(beer_names)
+
+        # beer_names_test = response.xpath("//h3[contains(@class, 's-component-content') and contains(@class, 's-font-heading')]/p/text()").extract_first()
+        # # print(beer_types)
+        # print(beer_names)
+        # print(beer_names_test)
+
+
+
--- Demo2/zips/zips/spiders/xlsx.py
+++ Demo2/zips/zips/spiders/xlsx.py
@@ -3,4 +3,4 @@ import xlrd
 
 df = pd.read_excel("C:/Users/Csipan/Desktop/Kapucinus.xlsx", sheet_name="Sheet1")
 print(df)
-df.to_json("kapucinus.json")
+df.to_json("kapucinusok.json")
